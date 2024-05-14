43ce5e64af14867611c09c52b53cd521ce8a0b38
Amil Bakhshaliev <a.e.bakhshaliiev@student.khai.edu>
2024-05-14 17:37:38 +0300

    Project

---
+++ Net_Lab_2/BotActions.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using static System.Net.WebRequestMethods;
+
+namespace Net_Lab_2
+{
+    public static class BotActions {
+
+        public static async Task GetDuckImageOrGif(ITelegramBotClient botClient, long chatId, CancellationToken token)
+        {
+            var root = "https://random-d.uk/api/random";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    try
+                    {
+                        HttpResponseMessage response = await client.GetAsync(root);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string jsonResult = await response.Content.ReadAsStringAsync();
+
+                            var duckData = JsonConvert.DeserializeObject<dynamic>(jsonResult);
+
+                            string imageUrl = duckData.url;
+                            Console.WriteLine($"Завантажена свiтлина: {imageUrl}");
+                            if (imageUrl != null)
+                            {
+                                await botClient.SendPhotoAsync(chatId,
+                                    photo: InputFile.FromUri(imageUrl),
+                                    caption: $"<b>DuckDuck!</b>\n<a href = '{imageUrl}'>Джерело</a>",
+                                    parseMode: ParseMode.Html,
+                                    cancellationToken: token);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Ошибка при загрузке изображения: {response.StatusCode}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ошибка при выполнении запроса: {ex.Message}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при выполнении запроса: {ex.Message}");
+                }
+            }
+        }
+    }
+}
+
---
+++ Net_Lab_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Net_Lab_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new TelegramBotClient("6706397450:AAHX0OHzhpgSNsIF6UhG1t58b2YGjYJaNNg");
+        client.StartReceiving(Update, Error);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"> Бота увiмкнено! ({DateTime.UtcNow} UTC)");
+
+            while (true)
+            {
+                string exitKeyword = "exit";
+
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == exitKeyword)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"> Бота вимкнено! ({DateTime.UtcNow} UTC)");
+                    break;
+                }
+                else
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+
+
+    static async Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+    {
+
+    }
+
+    static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+    {
+       
+        var message = update.Message;
+
+        if (message != null)
+        {
+            switch (message.Text) 
+            {
+                case "Give duck!":
+                    Console.WriteLine($"({message.Date.ToLocalTime()}) {message.Chat.Id}, {message.Chat.Username}: {message.Text}");
+                    await BotActions.GetDuckImageOrGif(botClient, message.Chat.Id, token);
+                    break;
+                default: botClient.SendTextMessageAsync(message.Chat.Id, "No duck :(");
+                    break;
+            }
+        }
+    }
+}
